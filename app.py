a02f2455cff51814b37bb6bee0abc6ab3f56c1a0
Lucero Simental <simentallucero733@gmail.com>
2025-09-19 08:32:52 -0600

    feat: get ip

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+import socket
 print("Hola soy Lucero")
+
+hostaname=socket.gethostname()
+print(f"hostname:{hostaname}")
+
+IPAddress = socket.gethostbyname(hostaname)
+print(f"IP Address: {IPAddress} ")
+
+
+
+
