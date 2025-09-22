8001ba8d4d18b1911d1f36e2f41b1e4d7d067709
Lucero Simental <simentallucero733@gmail.com>
2025-09-22 08:31:31 -0600

    feat: agrega funcion resta

--- app.py
+++ app.py
@@ -14,3 +14,5 @@ for i in range (10):
 numero_a = input("dame le primer numero")
 numero_b = input("dame le segundo numero")
 print(f"la suma es:{numero_a+numero_b}")
+
+print("resta",numero_a-numero_b)
